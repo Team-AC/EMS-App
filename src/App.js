6b64ca509c10cd03f87f41375f059d023b712d2a
Basil Mahmood <basilmahmood98@gmail.com>
2020-12-01 21:31:11 -0500

    Restructured App.js to prevent contents from going underneath nav drawer.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core';
+import { Container, CssBaseline, makeStyles } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
@@ -7,31 +7,45 @@ import Header from './components/Header';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from './components/About';
 import Simulation from './components/Simulation';
-import Snackbar from './components/Snackbar';
 
-export default () => (
-  <div className="App">
-    <Grid container direction="column" spacing={2}>
-      <Grid item>
-        <Header/>
-      </Grid>
-      
-      <Grid item>
-        <Grid container direction="row">
-          <Router>
-            <Grid item xs={2}>
-              <NavDrawer/>
-            </Grid>
-            <Grid item xs={10}>
+const drawerWidth = 280;
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    marginLeft: drawerWidth,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    margin: 0,
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+  <CssBaseline/>
+
+  return (
+    <div className="App">
+      <Header/>
+
+      <Router>
+          <NavDrawer drawerWidth={drawerWidth}/>
+
+          <main className={classes.content}>
+            <Container maxWidth="lg" className={classes.container}>
               <Switch>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/Simulation" component={Simulation}/>
                 <Route path="/About" component={About}/>
               </Switch>
-            </Grid>
-          </Router>
-        </Grid>
-      </Grid>
-    </Grid>
-  </div>
-);
+            </Container>
+          </main>
+
+      </Router>
+    </div>
+  )
+}
--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -1,42 +1,65 @@
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
 import { BatteryChargingFull, Business, EvStation, Inbox, Info, TouchAppOutlined } from '@material-ui/icons';
 import {Link} from "react-router-dom";
 
 
-export default () => (
-  <Drawer width={2} variant="permanent">
-    <br/><br/><br/>
-    <List>
-      <ListItem button component = {Link} to ="/">
-        <ListItemIcon>
-          <Business/>
-        </ListItemIcon>
-        <ListItemText primary="Energy Data for Building"/>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <EvStation/>
-        </ListItemIcon>
-        <ListItemText primary="Energy Data for EV"/>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <BatteryChargingFull/>
-        </ListItemIcon>
-        <ListItemText primary="Energy Data for BESS"/>
-      </ListItem>
-      <ListItem button component={Link} to="/Simulation">
-        <ListItemIcon>
-          <TouchAppOutlined/>
-        </ListItemIcon>
-        <ListItemText primary ="Simulation"/>
-      </ListItem>
-      <ListItem button component ={Link} to="/About">
-        <ListItemIcon>
-          <Info/>
-        </ListItemIcon>
-        <ListItemText primary="About"/> 
-      </ListItem>   
-    </List>
-  </Drawer>
-)
+export default (props) => {
+
+  const drawerWidth = props.drawerWidth; // Defined in App.js to shift all content leftwards based on width
+
+  const useStyles = makeStyles((theme) => ({
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Drawer 
+      className={classes.drawer}         
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      variant="permanent"
+    >
+      <br/><br/><br/>
+      <List>
+        <ListItem button component = {Link} to ="/">
+          <ListItemIcon>
+            <Business/>
+          </ListItemIcon>
+          <ListItemText primary="Energy Data for Building"/>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <EvStation/>
+          </ListItemIcon>
+          <ListItemText primary="Energy Data for EV"/>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <BatteryChargingFull/>
+          </ListItemIcon>
+          <ListItemText primary="Energy Data for BESS"/>
+        </ListItem>
+        <ListItem button component={Link} to="/Simulation">
+          <ListItemIcon>
+            <TouchAppOutlined/>
+          </ListItemIcon>
+          <ListItemText primary ="Simulation"/>
+        </ListItem>
+        <ListItem button component ={Link} to="/About">
+          <ListItemIcon>
+            <Info/>
+          </ListItemIcon>
+          <ListItemText primary="About"/> 
+        </ListItem>   
+      </List>
+    </Drawer>
+  )
+}
