c681bdd32d764ae7512461ef697da9edd8b079f0
Edward C <edwardcyu>
2021-02-03 19:25:09 -0500

    Added images to the home page

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,3 @@
-import { Button } from "@material-ui/core";
-
 export const ENQUEUE_SNACKBAR = 'ENQUEUE_SNACKBAR';
 export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
 export const REMOVE_SNACKBAR = 'REMOVE_SNACKBAR';
