da48ebb55b1de0991ff8b597a57e719881a3bb58
Edward C <edwardcyu>
2021-03-20 17:28:46 -0400

    Added pie chart and interval choosing for BESS page

--- src/components/EMS/BESS/Bess.js
+++ src/components/EMS/BESS/Bess.js
@@ -1,35 +1,66 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Button, ButtonGroup, Grid, Typography } from '@material-ui/core';
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
-//import BessPie from './BessPie';
-//import EvPie from './EvPie';
+import BessPie from './BessPie';
+import EvPie from './EvPie';
 
 export default function Bess() {
   const [dataInterval, setDataInterval] = useState('pastDay')
   const [data, setData] = useState([]);
-
   useEffect(() => {
     sendRequest(dataInterval);
-  }, [dataInterval])
+  }, [])
 
   const sendRequest = (e) => {
     axios.get(`/api/bess/energy`,)
       .then(res => {
         const interval = res.data[e]
-
+        const tempData = [
+          {
+            "id": "Bess Ev Charging",
+            "label": "Bess Ev Charging",
+            "value": interval.bess.ev,
+          },
+          {
+            "id": "Bess Arbitrage",
+            "label": "Bess Arbitrage",
+            "value": interval.bess.arbitrage,
+          },
+          {
+            "id": "Bess Load Shedding",
+            "label": "Bess Load Shedding",
+            "value": interval.bess.load,
+          },
+          {
+            "id": "Grid Ev Charging",
+            "label": "Grid Ev Charging",
+            "value": interval.grid.ev,
+          },   
+        ]
         setData(
-          interval.bess.ev,
-          interval.bess.arbitrage,
-          interval.bess.load,
-          interval.grid.ev,
+          tempData
         )
       })
   }
 
+  const changeInterval = (event) => {
+    setDataInterval(event.currentTarget.value);
+  }
   return (
     <Grid container spacing={3}>
-      <Typography> testing{data} </Typography>
+      <Grid item xs={12}>
+        <BessPie data={data} />
+      </Grid>
+      <Grid item xs={12}>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+          <Button value='pastYear' onClick={changeInterval}>Past Year</Button>
+          <Button value='past3Months' onClick={changeInterval}>Past 3 Months</Button>
+          <Button value='pastMonth' onClick={changeInterval}>Past Month</Button>
+          <Button value='pastWeek' onClick={changeInterval}>Past Week</Button>
+          <Button value='pastDay' onClick={changeInterval}>Past Day</Button>
+        </ButtonGroup>
+      </Grid>
     </Grid>
   )
 }
